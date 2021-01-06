c168325944be7c790010a8a734c9b8f14dcef048
Timur Gagiev <timur.gagiev@sperasoft.com>
2021-01-06 22:31:11 +0300

    initial commit, base project files structure

---
+++ src/FRMain/Pc/FRMain.cpp
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <stdio.h>
+#include "Resource.h"
+
+int main()
+{
+    return 0;
+}
---
+++ src/FRMain/Pc/Resource.h
@@ -0,0 +1,8 @@
+#define IDD_MAIN                       0x09
+#define IDD_ITEM_EDITOR                0x67
+#define IDD_EDIT_TRIGGERS              0x68
+#define IDD_NODE                       0x69
+#define IDD_EVENT                      0x6A
+#define IDD_TARGET                     0x6B
+#define IDD_PICK_MODEL                 0x6C
+#define IDD_NAME_DEBUG                 0x6D
